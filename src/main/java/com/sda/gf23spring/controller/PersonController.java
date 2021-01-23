4cbb78b9752d13982263a7938ad72418838857e3
uniko <uio6398@gmail.com>
2021-01-23 10:59:17 +0100

    add person

--- src/main/java/com/sda/gf23spring/controller/PersonController.java
+++ src/main/java/com/sda/gf23spring/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.sda.gf23spring.service.PersonService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,6 +24,19 @@ public class PersonController {
         List<Person> people = personService.getAll();
         model.addAttribute("persons", people);
         return "index";
+    }
+
+    @GetMapping("/sperson/id/{id}")
+    public String getPersonById(@PathVariable("id") int personId, Model model) {
+        Person byId = personService.getById(personId);
+        model.addAttribute("person", byId);
+        return "person";
+    }
+
+    @GetMapping("/addPerson")
+    public String addPerson(Model model) {
+        model.addAttribute("newPerson", new Person());
+        return "addPerson";
     }
 
 }
