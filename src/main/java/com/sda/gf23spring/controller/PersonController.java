7e00acc34ef4a9fb35968ff7002a2df09835ba28
uniko <uio6398@gmail.com>
2021-01-23 09:51:37 +0100

    thymeleaf start

--- src/main/java/com/sda/gf23spring/controller/PersonController.java
+++ src/main/java/com/sda/gf23spring/controller/PersonController.java
@@ -1,14 +1,17 @@
 package com.sda.gf23spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class PersonController {
 
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello.html";
+    @GetMapping("/spersons")
+    public String getAllPersons(Model model) {
+        model.addAttribute("hello", "Persons");
+        return "index";
     }
+
 }
