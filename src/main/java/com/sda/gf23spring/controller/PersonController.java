07145136a90fbc6359c9181fe86be9bc5280c61c
uniko <uio6398@gmail.com>
2021-01-23 10:15:30 +0100

    bootstrap

--- src/main/java/com/sda/gf23spring/controller/PersonController.java
+++ src/main/java/com/sda/gf23spring/controller/PersonController.java
@@ -1,16 +1,27 @@
 package com.sda.gf23spring.controller;
 
+import com.sda.gf23spring.person.Person;
+import com.sda.gf23spring.service.PersonService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 @Controller
 public class PersonController {
 
+    private PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
     @GetMapping("/spersons")
     public String getAllPersons(Model model) {
-        model.addAttribute("hello", "Persons");
+        List<Person> people = personService.getAll();
+        model.addAttribute("persons", people);
         return "index";
     }
 
